93b0692e1ca114481a588f27093551a5fb77e957
tony-lee <codelee0907@gmail.com>
2026-01-30 15:55:56 +0900

    update

--- static/js/main/service-introduce.js
+++ static/js/main/service-introduce.js
@@ -4,10 +4,12 @@ const readmeOpen = document.querySelectorAll(".readme_open");
 const contentsBox = document.querySelectorAll(".contents_box");
 
 listShow.forEach((show, idx) => {
-    show.addEventListener("click", (e) => {});
-    readmeOpen[idx].dataset.status =
-        readmeOpen[idx].dataset.status === "close" ? "open" : "close";
-    contentsBox[idx].classList.toggle("active");
+    show.addEventListener("click", (e) => {
+        // 이 안에 코드가 있어야 클릭할 때 실행됨!
+        readmeOpen[idx].dataset.states =
+            readmeOpen[idx].dataset.states === "close" ? "open" : "close";
+        contentsBox[idx].classList.toggle("active");
+    });
 });
 
 // 원픽공고 스크랩
@@ -15,6 +17,8 @@ const scrabStars = document.querySelectorAll(".rIcon.devAddScrap.devRecommend");
 
 scrabStars.forEach((scrabStar) => {
     scrabStar.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
         scrabStar.classList.toggle("str_off");
         scrabStar.classList.toggle("str_on");
     });
